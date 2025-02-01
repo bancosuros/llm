bc586596e8ad29c3ce07b53d38d9a45d9aa68651
bancosuros <banpara.suros@gmail.com>
2025-02-01 17:36:28 -0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -24,5 +24,5 @@ if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     response = client.chat.completions.create(model="gpt-3.5-turbo", messages=st.session_state.messages)
     msg = response.choices[0].message.content
-    st.session_state.messages.append({"role": "assistant", "content": msg})
+    st.session_state.messages.append({"role": "system", "content": msg})
     st.chat_message("assistant").write(msg)
