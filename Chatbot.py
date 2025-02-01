7d185a7e7c97d2711e968b8556a5ca5417090def
bancosuros <banpara.suros@gmail.com>
2025-02-01 17:25:58 -0300

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -16,7 +16,7 @@ for msg in st.session_state.messages:
 
 if prompt := st.chat_input():
     if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
+        st.info("Adicione sua chave de API OpenAI para continuar..")
         st.stop()
 
     client = OpenAI(api_key=openai_api_key)
